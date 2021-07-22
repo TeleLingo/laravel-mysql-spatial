0d92ad9adfc78f1c8cb3c68707337edce0aa5747
Christos Nikolakakis <nikolakakis.christos@gmail.com>
2021-07-22 18:37:52 +0300

    DB connection bugfix
    - The DB connection is started only when the application is not running in console mode.

--- src/SpatialServiceProvider.php
+++ src/SpatialServiceProvider.php
@@ -14,6 +14,7 @@ use Grimzy\LaravelMysqlSpatial\Doctrine\Point;
 use Grimzy\LaravelMysqlSpatial\Doctrine\Polygon;
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Database\DatabaseServiceProvider;
+use Illuminate\Support\Facades\App;
 
 /**
  * Class DatabaseServiceProvider.
@@ -27,36 +28,38 @@ class SpatialServiceProvider extends DatabaseServiceProvider
      */
     public function register()
     {
-        // The connection factory is used to create the actual connection instances on
-        // the database. We will inject the factory into the manager so that it may
-        // make the connections while they are actually needed and not of before.
-        $this->app->singleton('db.factory', function ($app) {
-            return new ConnectionFactory($app);
-        });
+        if (!App::runningInConsole()){
+                // The connection factory is used to create the actual connection instances on
+            // the database. We will inject the factory into the manager so that it may
+            // make the connections while they are actually needed and not of before.
+            $this->app->singleton('db.factory', function ($app) {
+                return new ConnectionFactory($app);
+            });
 
-        // The database manager is used to resolve various connections, since multiple
-        // connections might be managed. It also implements the connection resolver
-        // interface which may be used by other components requiring connections.
-        $this->app->singleton('db', function ($app) {
-            return new DatabaseManager($app, $app['db.factory']);
-        });
+            // The database manager is used to resolve various connections, since multiple
+            // connections might be managed. It also implements the connection resolver
+            // interface which may be used by other components requiring connections.
+            $this->app->singleton('db', function ($app) {
+                return new DatabaseManager($app, $app['db.factory']);
+            });
 
-        if (class_exists(DoctrineType::class)) {
-            // Prevent geometry type fields from throwing a 'type not found' error when changing them
-            $geometries = [
-                'geometry'           => Geometry::class,
-                'point'              => Point::class,
-                'linestring'         => LineString::class,
-                'polygon'            => Polygon::class,
-                'multipoint'         => MultiPoint::class,
-                'multilinestring'    => MultiLineString::class,
-                'multipolygon'       => MultiPolygon::class,
-                'geometrycollection' => GeometryCollection::class,
-            ];
-            $typeNames = array_keys(DoctrineType::getTypesMap());
-            foreach ($geometries as $type => $class) {
-                if (!in_array($type, $typeNames)) {
-                    DoctrineType::addType($type, $class);
+            if (class_exists(DoctrineType::class)) {
+                // Prevent geometry type fields from throwing a 'type not found' error when changing them
+                $geometries = [
+                    'geometry'           => Geometry::class,
+                    'point'              => Point::class,
+                    'linestring'         => LineString::class,
+                    'polygon'            => Polygon::class,
+                    'multipoint'         => MultiPoint::class,
+                    'multilinestring'    => MultiLineString::class,
+                    'multipolygon'       => MultiPolygon::class,
+                    'geometrycollection' => GeometryCollection::class,
+                ];
+                $typeNames = array_keys(DoctrineType::getTypesMap());
+                foreach ($geometries as $type => $class) {
+                    if (!in_array($type, $typeNames)) {
+                        DoctrineType::addType($type, $class);
+                    }
                 }
             }
         }
